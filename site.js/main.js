8a45b4e2b45cf6113790ded8015602f8f93d029d
nhharm <nhharm@gmail.com>
2022-10-03 17:33:26 -0400

    "commit"

--- site.js/main.js
+++ site.js/main.js
@@ -17,6 +17,15 @@ const negTrendOWGR = [];
 
 
 
+owgrObjects.forEach(player => { 
+  if (player.ranking - player.lastWeek >= 10){
+    posTrendOWGR.push(player)
+  }
+  if (player.ranking - player.lastWeek <= -10){
+    negTrendOWGR.push(player)
+  }
+});
+
 function tournamentParticipants(){
   tournListtNames = [];
   const owgrField = [];
@@ -36,7 +45,7 @@ function addSgTg(){
     return f.name === el.name ;
   });
 });**/
-const output = final.reduce((a, p) => {
+ output = final.reduce((a, p) => {
   const uP = sgTeeGreenObjects.find(u => u.name === p.name);
   if (uP) a.push({...p, sgtg:uP.rank});
   return a;
@@ -44,19 +53,24 @@ const output = final.reduce((a, p) => {
 console.log(output)
 }
 
-
-owgrObjects.forEach(player => { 
-    if (player.ranking - player.lastWeek >= 10){
-      posTrendOWGR.push(player)
-    }
-    if (player.ranking - player.lastWeek <= -10){
-      negTrendOWGR.push(player)
-    }
-});
 tournamentParticipants()
 addSgTg()
 /*
 owgr = 70
 teetogreen = 15
 pos=7.5
-neg=7.5 */
+neg=7.5 
+    Let v0=(1/w0),v1=(1/w1),v2=(1/w2),
+and let t=v0+v1+v2.
+Then simply scale v0,v1,v2 by
+u0=(v0/t),u1=(v1/t),u2=(v2/t).
+
+
+*/
+
+function getWeightedOutput(){
+  output.forEach(player => { 
+    let owgrVal = player.rank;
+    let shotsGainedVal = player.sgtg;
+  });
+}
